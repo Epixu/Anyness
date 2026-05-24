4cdc9e2814f2b7627574f64472af6dc29d650164
Epixu <dimomarkov@gmail.com>
2026-05-25 00:42:28 +0300

    Another attempt

--- source/components/Multiprovider.hpp
+++ source/components/Multiprovider.hpp
@@ -182,7 +182,7 @@ namespace Langulus::Anyness::Component
 
       /// Default-initialize the heap pointer                                 
       constexpr void ConstructDefault(this auto& self) noexcept {
-         Subcomponents::ForEach([&]<class C> noexcept {
+         Subcomponents::ForEach([&]<class C>() noexcept {
             self.C::ConstructDefault();
          });
       }
@@ -193,7 +193,7 @@ namespace Langulus::Anyness::Component
       ///      account only when we're cloning or copying, as only then       
       ///      a new allocation occurs.                                       
       void ConstructFrom(this auto& self, auto&& intent, size_t reserve = 0) {
-         Subcomponents::ForEach([&]<class C> {
+         Subcomponents::ForEach([&]<class C>() {
             self.C::ConstructFrom(LglsFwd(intent), reserve);
          });
       }
