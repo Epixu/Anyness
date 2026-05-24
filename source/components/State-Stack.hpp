fda5e33bd1f80cca0b3935bd45a4bd37e6981b2c
Epixu <dimomarkov@gmail.com>
2026-05-24 22:02:25 +0300

    Clang build fix

--- source/components/State-Stack.hpp
+++ source/components/State-Stack.hpp
@@ -152,7 +152,7 @@ namespace Langulus::Anyness::Component
 
       /// Check if container is in the default state                          
       ///   @return true if either contains state, or has stuff inserted      
-      constexpr bool IsDefaultState(this auto const& self) noexcept {
+      bool IsDefaultState(this auto const& self) noexcept {
          if constexpr (HasStates)
             return self.GetState().mState == self.GetDefaultState();
          else
